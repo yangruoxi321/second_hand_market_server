9c41b1be6b8c211d66e48fa8f7c1039fda2f5f4e
yangruoxi321 <yangruoxi321@gmail.com>
2024-04-01 20:49:31 -0400

    Initial commit

---
+++ src/main/java/com/example/second_hand_market_server/SecondHandMarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.second_hand_market_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandMarketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandMarketServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/second_hand_market_server/SecondHandMarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_hand_market_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandMarketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
