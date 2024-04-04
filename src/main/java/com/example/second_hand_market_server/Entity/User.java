30d3d3a0ea171b77e979f7400db02865c206ae84
yangruoxi321 <yangruoxi321@gmail.com>
2024-04-03 20:20:58 -0400

    Switch Id import in User entity
    The "Id" import in the User entity was switched from org.springframework.data.annotation.Id to jakarta.persistence.Id. This change was necessary to ensure consistency and compatibility within the Jakarta Persistence API.

--- src/main/java/com/example/second_hand_market_server/Entity/User.java
+++ src/main/java/com/example/second_hand_market_server/Entity/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Entity
