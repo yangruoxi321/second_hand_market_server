c87e29f3b4658a0ca5bc7b02263f523739d00f24
yangruoxi321 <yangruoxi321@gmail.com>
2024-04-09 13:54:54 -0400

    update

--- src/main/java/com/example/second_hand_market_server/Controller/userController.java
+++ src/main/java/com/example/second_hand_market_server/Controller/userController.java
@@ -1,23 +1,21 @@
 package com.example.second_hand_market_server.Controller;
 
 import com.example.second_hand_market_server.Entity.User;
-import com.example.second_hand_market_server.Respository.UserRepository;
 import com.example.second_hand_market_server.Service.UserService;
 import com.example.second_hand_market_server.model.RegisterBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class userController {
-    @Autowired
-    private UserRepository userRepository;
 
    @Autowired
     private UserService userService;
+
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
