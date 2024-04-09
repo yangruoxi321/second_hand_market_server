02324aa4391e5c60051aefc3509b4f1ffe8ce42d
yangruoxi321 <yangruoxi321@gmail.com>
2024-04-09 19:49:10 -0400

    1

--- src/main/java/com/example/second_hand_market_server/Controller/testTokenController.java
+++ src/main/java/com/example/second_hand_market_server/Controller/testTokenController.java
@@ -3,11 +3,14 @@ package com.example.second_hand_market_server.Controller;
 import com.example.second_hand_market_server.Service.TokenService;
 import com.example.second_hand_market_server.model.TokenBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class testTokenController {
     @Autowired
     private TokenService tokenService;
